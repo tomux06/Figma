d2dade6c166b5430c6474014e25b2f1cad55793e
tomux06 <tomux06@inbox.lt>
2023-03-14 18:26:16 +0200

    figma

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,17 @@ window.addEventListener('click', function(e){
 
     }
 });
-let 
+//Kontaktai
+let colorRed = document.getElementById('color');
+ console.log(colorRed.lastChild);
+ colorRed.lastChild.style.color = 'orange';
+
+
+ //Accordion
+ const accordion = document.getElementsByClassName('card-body');
+
+ for (i = 0; i< accordion.length; i++ ) {
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+    })
+ };
